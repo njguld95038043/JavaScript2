a7436286e91c731d0bfa40190a971bbd132be12a
t t <tt@tinoMacBook-Air.local>
2022-01-24 14:34:41 +0900

    [Finish]2

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-alert('Hello World');
+
+let max = 2;
+let num = 1000;
+let count = 0;
+
+while(num > max){
+  num = num / 2;
+  count = count + 1;
+}
+
+alert('2を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
